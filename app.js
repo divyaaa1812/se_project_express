cb1c84a0fadeca57af0a65a192d6da48c5ce810d
Divya B <divyaaa1812@gmail.com>
2023-10-05 22:47:02 -0700

    changes

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ const routes = require("./routes");
 app.use(express.json());
 app.use(routes);
 
+//middleware
+app.use((req, res, next) => {
+  req.user = {
+    _id: "651f98501f2952157b09cd18",
+  };
+  next();
+});
+
 //calling server by passing in port variable
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
