cb884671a5b639c7c959d9f445e1c61e4e7338b8
Divya B <divyaaa1812@gmail.com>
2023-10-26 15:52:51 -0700

    changes

--- controllers/clothingItem.js
+++ controllers/clothingItem.js
@@ -58,27 +58,27 @@ const deleteItem = (req, res) => {
           message: "CastError",
         });
       }
-      ClothingItem.findByIdAndDelete(itemId)
-        .orFail()
-        .then(() => {
-          res.status(statusCode.SUCCESS).send({ message: "200 Ok" });
-        })
-        .catch((err) => {
-          if (err.name === "DocumentNotFoundError") {
-            // send the error
-            res.status(statusCode.NOT_FOUND).send({
-              message: "Not found",
-            });
-          } else if (err.name === "CastError") {
-            res.status(statusCode.BAD_REQUEST).send({
-              message: "CastError",
-            });
-          } else {
-            res
-              .status(statusCode.DEFAULT)
-              .send({ message: "Error from delete item" });
-          }
+    });
+  ClothingItem.findByIdAndDelete(itemId)
+    .orFail()
+    .then(() => {
+      res.status(statusCode.SUCCESS).send({ message: "200 Ok" });
+    })
+    .catch((err) => {
+      if (err.name === "DocumentNotFoundError") {
+        // send the error
+        res.status(statusCode.NOT_FOUND).send({
+          message: "Not found",
         });
+      } else if (err.name === "CastError") {
+        res.status(statusCode.BAD_REQUEST).send({
+          message: "CastError",
+        });
+      } else {
+        res
+          .status(statusCode.DEFAULT)
+          .send({ message: "Error from delete item" });
+      }
     });
 };
 
