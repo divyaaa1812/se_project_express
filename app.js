ae93b6f9ffa570225b5a3e58fdb32632ab66b652
Divya B <divyaaa1812@gmail.com>
2023-11-11 21:47:38 -0800

    changes

--- app.js
+++ app.js
@@ -14,7 +14,12 @@ mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db", () => {
 
 // define routes
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: "*",
+  }),
+);
 app.use(routes);
 
 // calling server by passing in port variable
