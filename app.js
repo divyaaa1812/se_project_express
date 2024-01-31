cbcce45bebb45d28dd7e5bccc539207b72e16035
Divya B <divyaaa1812@gmail.com>
2024-01-31 14:32:46 -0800

    changes

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 // instance of express application
 const app = express();
-const { PORT = 3001 } = process.env.NODE_ENV;
+const { PORT = 3001 } = process.env;
 
 app.get("/crash-test", () => {
   setTimeout(() => {
